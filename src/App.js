b3d556a94981bc60829080b69ea11f28a8586a09
blaise <blaise.mbambo@gmail.com>
2022-08-18 11:03:02 +0100

    move login infos to App component, add route dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 import './App.css';
 import Login from './login/Login';
+import { useState, useEffect } from 'react'
 
 function App() {
+
+
+const CLIENT_ID = 'ce1f74efed6441dc89f7c8fea44230bc'
+const REDIRECT_URI = 'http://localhost:3000'
+const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize'
+const RESPONSE_TYPE = 'token'
+
+const [token,setToken] = useState('')
+
+useEffect(() =>{
+    const hash = window.location.hash
+    let token = window.localStorage.getItem('token')
+
+   if(!token && hash){
+        token = hash.substring(hash.indexOf('access_token=') + 'access_token='.length)
+        token = token.substring(0,token.indexOf('&'))
+
+        window.location.hash = ''
+        window.localStorage.setItem('token',token)
+    }
+    
+    setToken(token)
+
+})
+
+function logout() {
+  setToken('')
+  window.localStorage.removeItem('item')
+}
+
+let hrefAuthorizeLink = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`
+
+
   return (
     <div className="App">
-      <Login />
+      <Login hrefAuthorizeLink={hrefAuthorizeLink}/>
     </div>
   );
 }
--- src/login/Login.js
+++ src/login/Login.js
@@ -1,30 +1,6 @@
 import headphoneImg from '../images/headphones-transparent-png-16 1.png'
-import { useState, useEffect } from 'react'
 
-export default function Login() {
-
-    const CLIENT_ID = 'ce1f74efed6441dc89f7c8fea44230bc'
-    const REDIRECT_URI = 'http://localhost:3000'
-    const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize'
-    const RESPONSE_TYPE = 'token'
-
-    const [token,setToken] = useState('')
-
-    useEffect(() =>{
-        const hash = window.location.hash
-        let token = window.localStorage.getItem('token')
-
-       if(!token && hash){
-            token = hash.substring(hash.indexOf('access_token=') + 'access_token='.length)
-            token = token.substring(0,token.indexOf('&'))
-
-            window.location.hash = ''
-            window.localStorage.setItem('token',token)
-        }
-        
-        setToken(token)
-
-    })
+export default function Login({hrefAuthorizeLink}) {
 
     return (
         <div className="login">
@@ -39,7 +15,7 @@ export default function Login() {
         en terme de musique.</h1>
                     </div>
                     <div className='login-btn-container'>
-                        <a className='login-btn' href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>CONNEXION</a>
+                        <a className='login-btn' href={hrefAuthorizeLink}>CONNEXION</a>
                     </div>
                 </div>
             </div>

fa230fb753055bd99c860c168d5ca1b3f1641dab
blaise <blaise.mbambo@gmail.com>
2022-09-08 17:20:57 +0100

    token problem fixed2

--- src/pages/Components/SpotifyPlayerComponent.js
+++ src/pages/Components/SpotifyPlayerComponent.js
@@ -5,7 +5,7 @@ import { tokenContext } from '../../App'
 export default function SpotifyPlayerComponent({uri}){
 
     const token = useContext(tokenContext)
-    token = token.token
+    
     return(
         <div className="player-container">
             <SpotifyPlayer autoPlay={true} initialVolume={10} play={true}
